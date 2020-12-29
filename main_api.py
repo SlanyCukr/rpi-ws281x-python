085e67d5507f9f70670a290acbb2cbcc7cf27ef3
Marek Hovadík <hovadik@atlasgroup.cz>
2020-12-29 21:52:14 +0100

    Flask hello world example

--- main_api.py
+++ main_api.py
@@ -1,4 +1,14 @@
-import flask
+from flask import Flask
 
 import led_control
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
