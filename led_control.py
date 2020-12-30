48e88d27ade20ae08e97dc2cc28fa74c0b886d14
Marek Hovadík <hovadik@atlasgroup.cz>
2020-12-30 08:09:35 +0100

    Only one function will be running at the time

--- led_control.py
+++ led_control.py
@@ -19,30 +19,83 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 strip = get_strip()
+functions_status = {
+    'led_rainbow': False,
+    'led_turn_off': False,
+    'led_gradually_turn_on': False
+}
 
 
-def led_turn_off(wait_ms=50):
+def only_run_one(function_name: str):
+    """
+    Sets the status for each function.
+    """
+    for key in functions_status:
+        if key == function_name:
+            functions_status[key] = True
+        else:
+            functions_status[key] = False
+
+
+def wheel(pos):
+    """
+    Generate rainbow colors across 0-255 positions.
+    """
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+
+def led_rainbow(wait_ms=20):
+    """
+    Draw rainbow that uniformly distributes itself across all pixels.
+    """
+    only_run_one('led_rainbow')
+
+    while True:
+        for j in range(256):
+            for i in range(strip.numPixels()):
+                if not functions_status['led_rainbow']:
+                    return
+                strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+
+
+def led_turn_off():
     """
     Turn off the LED strip.
     """
+    only_run_one('led_turn_off')
+
     black = Color(0, 0, 0)
     for i in range(strip.numPixels()):
+        if not functions_status['led_turn_off']:
+            return
         strip.setPixelColor(i, black)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
+    strip.show()
 
 
 def led_gradually_turn_on(wait_ms=50):
     """
     Gradually increase brightness.
     """
-    color_red = 255
-    color_green = 255
-    color_blue = 255
+    only_run_one('led_gradually_turn_on')
+
+    color_red = 0
+    color_green = 0
+    color_blue = 0
     for i in range(255):
         for j in range(strip.numPixels()):
+            if not functions_status['led_gradually_turn_on']:
+                return
             color = Color(max(0, color_red + i + 1), max(0, color_green + i + 1), max(0, color_blue + i + 1))
             strip.setPixelColor(j, color)
-            #strip.show()
+            strip.show()
         strip.show()
         time.sleep(wait_ms / 1000.0)
--- main_api.py
+++ main_api.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from led_control import led_gradually_turn_on, led_turn_off
+from led_control import led_gradually_turn_on, led_turn_off, led_rainbow
 
 app = Flask(__name__)
 
@@ -17,5 +17,11 @@ def turn_off():
     return 'Turned off!'
 
 
+@app.route('/rainbow')
+def rainbow():
+    led_rainbow()
+    return 'Rainbow!'
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
