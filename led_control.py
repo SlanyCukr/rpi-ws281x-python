fbb4920b16cafa30a78f38c6fb30310a7fb8c8ef
Marek Hovadík <hovadik@atlasgroup.cz>
2021-01-02 14:50:23 +0100

    Running LED code in separate process - the API returns faster, new endpoint - set_brightness

--- led_control.py
+++ led_control.py
@@ -20,22 +20,15 @@ LED_BRIGHTNESS = 64     # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 strip = get_strip()
-functions_status = {
-    'led_rainbow': False,
-    'led_turn_off': False,
-    'led_gradually_turn_on': False
-}
 
 
-def only_run_one(function_name: str):
+def led_set_brightness(brightness: int):
     """
-    Sets the status for each function.
+    Sets brightness for the strip.
     """
-    for key in functions_status:
-        if key == function_name:
-            functions_status[key] = True
-        else:
-            functions_status[key] = False
+    global LED_BRIGHTNESS, strip
+    LED_BRIGHTNESS = brightness
+    strip = get_strip()
 
 
 def wheel(pos):
@@ -56,13 +49,9 @@ def led_rainbow(wait_ms=20):
     """
     Draw rainbow that uniformly distributes itself across all pixels.
     """
-    only_run_one('led_rainbow')
-
     while True:
         for j in range(256):
             for i in range(strip.numPixels()):
-                if not functions_status['led_rainbow']:
-                    return
                 strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
             strip.show()
             time.sleep(wait_ms/1000.0)
@@ -72,12 +61,8 @@ def led_turn_off():
     """
     Turn off the LED strip.
     """
-    only_run_one('led_turn_off')
-
     black = Color(0, 0, 0)
     for i in range(strip.numPixels()):
-        if not functions_status['led_turn_off']:
-            return
         strip.setPixelColor(i, black)
         strip.show()
 
@@ -89,15 +74,11 @@ def led_gradually_turn_on(timespan_sec1=300, timespan_sec2=300):
     wait_ms1 = ((timespan_sec1 / 255.0) / strip.numPixels())
     wait_ms2 = (timespan_sec2 / 255.0)
 
-    only_run_one('led_gradually_turn_on')
-
     color_red = 0
     color_green = 0
     color_blue = 0
     for i in range(255):
         for j in range(strip.numPixels()):
-            if not functions_status['led_gradually_turn_on']:
-                return
             color = Color(max(0, color_red + i + 1), max(0, color_green + i + 1), max(0, color_blue + i + 1))
             strip.setPixelColor(j, color)
             if wait_ms1 != 0:
--- main_api.py
+++ main_api.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
+import multiprocessing
 
-from led_control import led_gradually_turn_on, led_turn_off, led_rainbow
+from led_control import led_gradually_turn_on, led_turn_off, led_rainbow, led_set_brightness
 
 app = Flask(__name__)
+current_process = None
+current_target = None
 
 
 @app.route('/turn_on', methods=['POST'])
@@ -14,20 +17,56 @@ def turn_on():
     if 'timespan_sec2' in request.args:
         timespan_sec2 = int(request.args.get('timespan_sec2'))
 
-    led_gradually_turn_on(timespan_sec1, timespan_sec2)
-    return 'Turned on!'
+    # terminate old process, start a new one
+    global current_process, current_target
+    current_process.terminate()
+    current_target = led_gradually_turn_on
+    current_process = multiprocessing.Process(target=led_gradually_turn_on, args=(timespan_sec1, timespan_sec2))
+    current_process.start()
+
+    return 'Turning on!'
 
 
 @app.route('/turn_off', methods=['POST'])
 def turn_off():
-    led_turn_off()
-    return 'Turned off!'
+    # terminate old process, start a new one
+    global current_process, current_target
+    current_process.terminate()
+    current_target = led_turn_off
+    current_process = multiprocessing.Process(target=led_turn_off)
+    current_process.start()
+
+    return 'Turning off!'
 
 
 @app.route('/rainbow', methods=['POST'])
 def rainbow():
-    led_rainbow()
-    return 'Rainbow!'
+    # terminate old process, start a new one
+    global current_process, current_target
+    current_process.terminate()
+    current_target = led_rainbow
+    current_process = multiprocessing.Process(target=led_rainbow)
+    current_process.start()
+
+    return 'Running rainbow program!'
+
+
+@app.route('/set_brightness', methods=['POST'])
+def set_brightness():
+    global current_process
+    if 'brightness' not in request.args:
+        return "No brightness supplied.", 400
+
+    # set new brightness
+    brightness = int(request.args['brightness'])
+    current_process.terminate()
+    led_set_brightness(brightness)
+
+    # restart the running process
+
+    current_process = multiprocessing.Process(target=current_target)
+    current_process.start()
+    return "Brightness correctly set."
 
 
 @app.route('/hello')
@@ -36,4 +75,8 @@ def hello():
 
 
 if __name__ == '__main__':
+    current_target = led_rainbow
+    current_process = multiprocessing.Process(target=led_rainbow)
+    current_process.start()
+
     app.run(host='0.0.0.0', port=5000)
